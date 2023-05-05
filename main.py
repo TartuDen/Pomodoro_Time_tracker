683916cf66b34b7e10d6d1c32474ff90019dfa05
Pomogadryg <denver1033@gmail.com>
2023-05-05 08:22:52 +0300

    finalized the script

--- main.py
+++ main.py
@@ -9,48 +9,77 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+TIME_TRACK = 1
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def counting_checkmarks(local_tracker):
+    if local_tracker ==1:
+        label_checkmarks.config(text="✓")
+    elif local_tracker ==3:
+        label_checkmarks.config(text="✓"*2)
+    elif local_tracker ==5:
+        label_checkmarks.config(text="✓"*3) 
 
+def start_timer():
+    global TIME_TRACK
+    if TIME_TRACK <= 6:
+        if TIME_TRACK % 2 != 0:
+            label_work_break.config(text="Work!")
+            countdown(15)
+            counting_checkmarks(TIME_TRACK)
+        elif TIME_TRACK % 2 ==0:
+            label_work_break.config(text="Break")
+            if TIME_TRACK == 6:
+                countdown(10)
+            else:
+                countdown(5)
+        # print("time track: ", TIME_TRACK)
+        TIME_TRACK +=1
+    
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(time_):
 
+    mins, secs = divmod(time_, 60)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print(timer, end="\r")
+    canvas.itemconfig(text_id, text=timer)
+    if time_>0:
+        new_window.after(100,countdown, time_ - 1)
+    else:
+        start_timer()
+    # print("time_: ", time_)
+  
+    
+# ✓
 # ---------------------------- UI SETUP ------------------------------- #
 
 new_window = Tk()
 new_window.config(padx=50,pady=20)
 new_window.title("POMODORO - be more productive!")
-new_window.config(bg = "lightblue")
+new_window.config(bg = YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file=".\\tomato.png")
-canvas.create_image(103, 112, image = img)
+canvas.create_image(103, 100, image = img)
 canvas.grid(row=5, column=2)
 
+text_id = canvas.create_text(103,135,text="00:00", fill="white",font=(FONT_NAME,24,"bold"))
 
 
-
-
-label_work_break = Label(text="Work",font=(FONT_NAME,24))
+label_work_break = Label(text="",font=(FONT_NAME,24, "bold"), bg=YELLOW, fg=GREEN)
 label_work_break.grid(row = 3, column=2)
 
-label_counter = Label(text="counter")
-label_counter.grid(row=5, column=2)
+label_checkmarks = Label(text="", font=(FONT_NAME,20,"bold"), bg=YELLOW, fg=GREEN)
+label_checkmarks.grid(row=8,column=2)
 
-button_start = Button(text = "Start")
+button_start = Button(text = "Start", command=start_timer)
 button_start.grid(row=7, column=1)
 
 
 button_reset = Button(text="Reset")
 button_reset.grid(row=7, column=3)
-
-
-label_checkmarks = Label(text="Checkmarks")
-label_checkmarks.grid(row=8,column=2)
-
-
-
 
 
 
